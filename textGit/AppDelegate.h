aeb15f72dadb33157fde833df59aa9f4fe9f9c6f
saber <297686728@qq.com>
2015-10-20 10:21:27 +0800

    first commit

---
+++ textGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  textGit
+//
+//  Created by MS on 15-10-20.
+//  Copyright (c) 2015年 Saber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ textGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  textGit
+//
+//  Created by MS on 15-10-20.
+//  Copyright (c) 2015年 Saber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
