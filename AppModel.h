39bedcc6e16303e1fa09d3954f25a6eb2b9d89cf
saber <297686728@qq.com>
2015-10-20 10:44:32 +0800

    hey

---
+++ AppModel.h
@@ -0,0 +1,13 @@
+//
+//  AppModel.h
+//  textGit
+//
+//  Created by MS on 15-10-20.
+//  Copyright (c) 2015年 Saber. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AppModel : NSObject
+
+@end
---
+++ AppModel.m
@@ -0,0 +1,13 @@
+//
+//  AppModel.m
+//  textGit
+//
+//  Created by MS on 15-10-20.
+//  Copyright (c) 2015年 Saber. All rights reserved.
+//
+
+#import "AppModel.h"
+
+@implementation AppModel
+
+@end
